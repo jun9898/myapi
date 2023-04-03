0bbb0738b145af101ff9821ffebdf039c28e8aaa
jun9898 <nice1998@gmail.com>
2023-04-03 22:20:46 +0900

    question_router 파일의 작성과
    router = APIRouter(
        prefix="/api/question",
    )
    
    코드의 대한 이해, 라우터의 작동 원리를 학습하였다.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from domain.question import question_router
+
 app = FastAPI()
 
 origins = [
-    "http://localhost:5174",    # 또는 "http://localhost:5173"
+    "http://localhost:5173",    # 또는 "http://localhost:5173"
 ]
 
 app.add_middleware(
@@ -16,6 +18,8 @@ app.add_middleware(
 )
 
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요 파이보"}
+# @app.get("/hello")
+# def hello():
+#     return {"message": "안녕하세요 파이보"}
+
+app.include_router(question_router.router)
