3d7465c43e23b4c830aaa892a9115ce436c8f910
jun9898 <nice1998@gmail.com>
2023-04-03 22:47:46 +0900

    question_router 파일에서 계속해서 세션을 열고 닫아주는 번거로운 작업을 덜어내기 위해 Depend를 사용하여 의존성을 주입해 코드를 간소화시키고 생산성을 증가시켰다.

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+# import contextlib 주석처리를 한 이유는 Depends(get_db)와 이중으로 적용되어 오류가 발생하기 때문이다.
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,3 +12,12 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+# @contextlib.contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from database import SessionLocal
+# from database import SessionLocal
+
+from database import get_db
 from models import Question
 
 router = APIRouter(
@@ -9,8 +12,13 @@ router = APIRouter(
 
 
 @router.get("/list")
-def question_list():
-    db = SessionLocal()
+def question_list(db: Session = Depends(get_db)):  #FastAPI의 Depends는 매개 변수로 전달 받은 함수를 실행시킨 결과를 리턴한다.
+
+    # db = SessionLocal()
+    # _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    # db.close() ----------- database에 작업이 시작되면 세션을 열어주고 완료되면 자동으로 세션을 닫아주는 get_db 함수를 등록하여 코드 작성 마지막에 db.close()를 입력하지 않아도 됩니다.
+
+    # with get_db() as db: # db 세션 객체를 사용한다.
+
     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    db.close()
     return _question_list
