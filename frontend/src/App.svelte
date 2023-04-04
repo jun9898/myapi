6ab332aecb498cade37dd5e07ef8bb52575e16a2
jun9898 <nice1998@gmail.com>
2023-04-04 22:40:52 +0900

    api.js의 fastapi 함수에 대해 다시한번 공부해 봐야 할 것 같음

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Router from 'svelte-spa-router'
-  import Hoem  from './routes/Hoem.svelte'
+  import Hoem  from './routes/Home.svelte'
 
   const routes = {
     '/': Hoem,
---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+    import fastapi from "../lib/api"
+    let question_list = []
+
+    function get_question_list() {
+    // fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
+    //   response.json().then((json) => { ----------api.js의 fastapi 함수를 사용해서 코드를 수정하였다 fastapi의 대한 이해가 좀 더 필요할 것 같다.
+        fastapi('get', '/api/question/list', {}, (json) => {
+        question_list = json;
+        })
+    }
+  
+
+    get_question_list()
+</script>
+
+<ul>
+  {#each question_list as question}
+    <li>{question.subject}</li>
+  {/each}
+</ul>
